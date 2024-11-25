f8a7e7f965544047fedf87c739839b4315af893e
thuhong <vothuhong2104@gmail.com>
2024-11-25 11:44:39 +0700

    Initial commit

---
+++ src/bai1/BTtrenlop.java
@@ -0,0 +1,29 @@
+package bai1;
+
+import java.util.Scanner;
+
+public class BTtrenlop {
+    public static void main(String[] args) {
+//        System.out.println("Nhập họ và tên:");
+//        String name = new Scanner(System.in).nextLine();
+//        System.out.println("Họ và tên:" + name);
+//
+//        System.out.println("Nhâp quê quán:");
+//        String country = new Scanner(System.in).nextLine();
+//        System.out.println("quê quán:" + country);
+//
+//        System.out.println("Nhập trường học:");
+//        String school = new Scanner(System.in).nextLine();
+//        System.out.println("Trường học:" + school);
+
+//bài 2
+        System.out.println("nhap hai số :");
+        int a = new Scanner(System.in).nextInt();
+        int b = new Scanner(System.in).nextInt();
+        System.out.println("Tong" + (a+b));
+        System.out.println("Tich" + (a*b));
+        System.out.println("Thuong" + (a/b));
+        System.out.println("Chia du" + (a%b));
+
+    }
+}
